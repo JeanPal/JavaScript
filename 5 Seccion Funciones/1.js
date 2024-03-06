1003d1def10a5adb38381a375beef62ff8e8228f
JeanPal <160547687+JeanPal@users.noreply.github.com>
2024-03-06 08:48:35 -0500

    Update 1.js

--- 5 Seccion Funciones/1.js
+++ 5 Seccion Funciones/1.js
@@ -1,18 +1,22 @@
 const resultado = (dato1) => {
-    if (dato1 % 2 === 0) {
+    if (!(dato1 % 2)) {
         return `${dato1} Es par`;
     }else {
         return `${dato1} Es impar`;
     }
 }
+
+
 do {
     dato1 = prompt(`Ingrese un nummero`);
 
-    if (dato1.trim() === '') {
+    let stringConTrim = dato1.trim()
+
+    if (!stringConTrim) {
         alert('No puedes dejar espacios vacios');
-    } else if (isNaN(dato1.trim())) {
+    } else if (isNaN(stringConTrim)) {
         alert('Debe ingresar un número no letra o caracteres especiales');
-    } else if (+dato1.trim() <= 0) {
+    } else if (+stringConTrim <= 0) {
         alert('Debe ingresar un número mayor que cero');
     }
 
