0d3b2bdc93aa8bb573a92e11ef71db067cd0a80d
JeanPal <160547687+JeanPal@users.noreply.github.com>
2024-03-06 08:50:30 -0500

    Update 4.js

--- 5 Seccion Funciones/4.js
+++ 5 Seccion Funciones/4.js
@@ -5,11 +5,14 @@ function numerosletras(numero) {
 
 do {
     numero = prompt(`Ingrese una numero del 1 al 10`);
-    if (numero.trim() === '') {
+
+    let stringConTrim = numero.trim();
+
+    if (stringConTrim) {
         alert('No puedes dejar espacios vacios');
-    } else if (isNaN(numero.trim())) {
+    } else if (isNaN(stringConTrim)) {
         alert('Debe ingresar un número no letra o caracteres especiales');
-    } else if (numero.trim() <= 0) {
+    } else if (stringConTrim <= 0) {
         alert('Debe ingresar un número mayor que cero');
     }
 } while (numero.trim() === '' || isNaN(numero.trim()) || +numero.trim() <= 0 );
